4e88c3ae908a3ae305e7db7342b84918e7dfdafd
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-08 14:49:14 +0530

    Largest Element

--- src/main/java/com/bridgelabz/arrayprogram/ArrayProgram.java
+++ src/main/java/com/bridgelabz/arrayprogram/ArrayProgram.java
@@ -3,12 +3,18 @@ package com.bridgelabz.arrayprogram;
 public class ArrayProgram {
     public static void main(String[] args) {
         int [] array= {3,4,5,7,10};
-        System.out.println("Array Element:");
-
+        int max= array[0];
         for(int i =0; i <array.length; i++){
             System.out.println(array[i]);
 
+            for (int j=1; i< array.length; i++){
+                if(array[j] > max){
+                    max=array[j];
+                }
+            }
 
+            System.out.println("Array Element:");
+            System.out.println("Largest Element : " +max);
         }
 
     }
