5c3776dea04c12d2598d335a71c205d6046f66f2
vaishnavi16-12 <vaishnavic16502@gmail.com>
2024-10-08 14:37:46 +0530

    Array Element

---
+++ src/main/java/com/bridgelabz/arrayprogram/ArrayProgram.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.arrayprogram;
+
+public class ArrayProgram {
+    public static void main(String[] args) {
+        int [] array= {3,4,5,7,10};
+        System.out.println("Array Element:");
+
+        for(int i =0; i <array.length; i++){
+            System.out.println(array[i]);
+
+
+        }
+
+    }
+}
